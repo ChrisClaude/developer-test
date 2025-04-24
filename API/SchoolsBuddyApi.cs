6834c9373f41c0ffbe4dc0da3c207096c259fdc0
Chris Claude <christ.tchambila@gmail.com>
2025-04-24 12:01:21 +0200

    Update SchoolsBuddyApi.cs

--- API/SchoolsBuddyApi.cs
+++ API/SchoolsBuddyApi.cs
@@ -10,18 +10,18 @@ internal static class SchoolsBuddyApi
         var api = app.MapGroup("api");
 
         api.MapGet("/Users", GetUsersAsync);
-        api.MapGet("/Icons/{name:string}", GetIconByNameAsync);
+        api.MapGet("/Icons/{name}", GetIconByNameAsync);
 
         return api;
     }
 
-    public static async Task<Ok<IEnumerable<UserDto>>> GetUsersAsync([AsParameters] IUserService userService)
+    public static async Task<Ok<IEnumerable<UserDto>>> GetUsersAsync(IUserService userService)
     {
         var users = await userService.GetUsersAsync();
         return TypedResults.Ok(users);
     }
 
-    public static async Task<Ok<byte[]?>> GetIconByNameAsync(string name, [AsParameters] IIconService iconService)
+    public static async Task<Ok<byte[]?>> GetIconByNameAsync(string name, IIconService iconService)
     {
         var icon = await iconService.GetIconBytesAsync(name);
         return TypedResults.Ok<byte[]?>(icon);
